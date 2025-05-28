397f5455889cff5bbd4f5d6622362856fbe556e4
Cody Keats <cocopuff2u@yahoo.com>
2025-05-28 17:52:24 -0400

    Setting up google

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -3,11 +3,11 @@ if (dnt != "1" && dnt != "yes") {
     // Replace analytics.js with gtag.js (latest Google Analytics)
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX'; // <-- Replace with your Measurement ID
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E0PB4Q5QGG'; // <-- Replace with your Measurement ID
     document.head.appendChild(script);
 
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-XXXXXXXXXX'); // <-- Replace with your Measurement ID
+    gtag('config', 'G-E0PB4Q5QGG'); // <-- Replace with your Measurement ID
 }
